3377f0da019f7258773b8217500b1a8e9ba01145
jankee94 <benitezjan94@gmail.com>
2026-06-11 08:28:14 -0500

    FINAL: Version completa con rotacion Gemini y Groq - 20260611082814

--- main.py
+++ main.py
@@ -215,3 +215,5 @@ elif menu == "Chat IA":
 
 st.markdown("---")
 st.markdown("<p style='text-align:center'>SG-SST PHVA | JAN BENITEZ</p>", unsafe_allow_html=True)
+
+# Force update - 06/11/2026 08:28:14
